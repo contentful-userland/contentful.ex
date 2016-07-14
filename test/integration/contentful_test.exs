58d266e386c944346b0fc73df068787ceb21ecd0
Nicolas Charlery <nicolas.charlery@gmail.com>
2016-07-14 13:25:14 -0400

    Fix Test Indentation

--- test/integration/contentful_test.exs
+++ test/integration/contentful_test.exs
@@ -19,54 +19,54 @@ defmodule Contentful.DeliveryTest do
 
   test "entry" do
     use_cassette "entry" do
-    entry = Delivery.entry(@space_id, @access_token, "5JQ715oDQW68k8EiEuKOk8")
+      entry = Delivery.entry(@space_id, @access_token, "5JQ715oDQW68k8EiEuKOk8")
 
-    assert is_map(entry["fields"])
+      assert is_map(entry["fields"])
     end
   end
 
   test "content_types" do
     use_cassette "content_types" do
-    first_content_type = Delivery.content_types(@space_id, @access_token)
-    |> List.first
+      first_content_type = Delivery.content_types(@space_id, @access_token)
+      |> List.first
 
-    assert is_list(first_content_type["fields"])
+      assert is_list(first_content_type["fields"])
     end
   end
 
   test "content_type" do
     use_cassette "content_type" do
-    content_type = Delivery.content_type(@space_id, @access_token, "1kUEViTN4EmGiEaaeC6ouY")
+      content_type = Delivery.content_type(@space_id, @access_token, "1kUEViTN4EmGiEaaeC6ouY")
 
-    assert is_list(content_type["fields"])
+      assert is_list(content_type["fields"])
     end
   end
 
   test "assets" do
     use_cassette "assets" do
-    first_asset = Delivery.assets(@space_id, @access_token)
-    |> List.first
+      first_asset = Delivery.assets(@space_id, @access_token)
+      |> List.first
 
-    assert is_map(first_asset["fields"])
+      assert is_map(first_asset["fields"])
     end
   end
 
   test "asset" do
     use_cassette "asset" do
-    asset = Delivery.asset(@space_id, @access_token, "2ReMHJhXoAcy4AyamgsgwQ")
-    fields = asset["fields"]
+      asset = Delivery.asset(@space_id, @access_token, "2ReMHJhXoAcy4AyamgsgwQ")
+      fields = asset["fields"]
 
-    assert is_map(fields)
+      assert is_map(fields)
     end
   end
 
   test "space" do
     use_cassette "space" do
-    space = Delivery.space(@space_id, @access_token)
-    locales = space["locales"]
-    |> List.first
+      space = Delivery.space(@space_id, @access_token)
+      locales = space["locales"]
+      |> List.first
 
-    assert locales["code"] == "en-US"
+      assert locales["code"] == "en-US"
     end
   end
 end

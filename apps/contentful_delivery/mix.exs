696653292f7eff979a80b946a75cfa1c14089f31
Florian Kraft <florian.kraft@contentful.com>
2020-03-11 23:37:43 +0100

    adds contentful as dependency for delivery

--- apps/contentful_delivery/mix.exs
+++ apps/contentful_delivery/mix.exs
@@ -30,7 +30,7 @@ defmodule Contentful.Delivery.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:contentful, in_umbrella: true}
     ]
   end
 end

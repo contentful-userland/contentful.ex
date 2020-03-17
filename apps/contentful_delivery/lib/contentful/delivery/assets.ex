31f51434453ad50effd5b26c981c48c2e0305e87
Florian Kraft <florian.kraft@contentful.com>
2020-03-17 20:00:23 +0100

    adds streamability to assets

--- apps/contentful_delivery/lib/contentful/delivery/assets.ex
+++ apps/contentful_delivery/lib/contentful/delivery/assets.ex
@@ -5,8 +5,10 @@ defmodule Contentful.Delivery.Assets do
   https://www.contentful.com/developers/docs/references/content-delivery-api/#/reference/assets
   """
 
-  alias Contentful.{Asset, Delivery, Space}
+  alias Contentful.{Asset, Collection, CollectionStream, Delivery, Space}
 
+  @behaviour Collection
+  @behaviour CollectionStream
   @doc """
   fetches one asset from a given space
 
@@ -14,10 +16,7 @@ defmodule Contentful.Delivery.Assets do
       space = "my_space_id"
       {:ok, %Asset{} = asset} =  space |>Contentful.Delivery.Assets.fetch_one("<asset_id>")
   """
-  @spec fetch_one(String.t() | Space.t(), String.t(), String.t() | nil) ::
-          {:ok, Asset.t()}
-          | {:error, atom(), original_message: String.t()}
-          | {:error, :unknown}
+  @impl Collection
   def fetch_one(space, asset, env \\ "master", api_key \\ nil)
 
   def fetch_one(%Space{meta_data: %{id: space_id}}, asset_id, env, api_key) do
@@ -32,14 +31,14 @@ defmodule Contentful.Delivery.Assets do
   end
 
   @doc """
-  Fetches all assets for a given Contentful.Space.
+  Fetches all assets for a given `Contentful.Space`.
 
   Will take basic collection filters into account, specifically :limit and :skip to traverse and 
-  limit the collection of entries.
+  limit the collection of assets.
 
   ## Examples
       space = "my_space_id"
-      {:ok, [%Asset{} | _]} = space |> Contentful.Delivery.Assets.fetch_all()
+      {:ok, [%Asset{} | _]} = space |> Assets.fetch_all()
 
       {:ok, [
         %Asset{ meta_data: %{ id: "foobar_0"}}, 
@@ -60,15 +59,7 @@ defmodule Contentful.Delivery.Assets do
         %Asset{ meta_data: %{ id: "foobar_2"}}
       ], total: 3} = space |> Assets.fetch_all(limit: 1, skip: 2)
   """
-  @spec fetch_all(
-          Space.t() | String.t(),
-          list(keyword()),
-          String.t(),
-          String.t() | nil
-        ) ::
-          {:ok, list(Contentful.Asset.t()), total: integer()}
-          | {:error, atom(), original_message: String.t()}
-          | {:error, :unknown}
+  @impl Collection
   def fetch_all(space, options \\ [], env \\ "master", api_key \\ nil)
 
   def fetch_all(%Space{meta_data: %{id: id}}, options, env, api_key) do
@@ -80,6 +71,44 @@ defmodule Contentful.Delivery.Assets do
 
   def fetch_all(space_id, options, env, api_key) do
     fetch_all(%Space{meta_data: %{id: space_id}}, options, env, api_key)
+  end
+
+  @doc """
+  Constructs a stream around __all__ assets of a `Contentful.Space` __that are published__.
+
+  Will return a stream of assets that can be composed with the standard libraries functions. 
+  This function calls the API endpoint for entries on demand, e.g. until the upper limit 
+  (the total of all assets) is reached.
+
+  __Warning__: With very large asset collections, this can quickly run into the request limit of the API! 
+
+  ## Examples
+      space = "my_space_id"
+      # API calls calculated by the stream (in this case two calls)
+      ["first_asset_id", "second_asset_id"] 
+        = space 
+          |> Assets.stream(limit: 1) 
+          |> Stream.map(fn %{ meta_data: %{ id: id }} -> id end) 
+          |> Enum.take(2) 
+
+      environment = "staging"
+      api_token = "foobar?foob4r"
+      ["first_asset_id"] 
+        = space 
+          |> Assets.stream(limit: 1, environment, api_token) 
+          |> Stream.map(fn %{ meta_data: %{ id: id }} -> id end) 
+          |> Enum.take(2) 
+    
+      # Use the :limit parameter to set the page size 
+      ["first_asset_id", "second_asset_id", "third_asset_id", "fourth_asset_id"] 
+        = space 
+          |> Assets.stream(limit: 4) 
+          |> Stream.map(fn %{ meta_data: %{ id: id }} -> id end) 
+          |> Enum.take(4) 
+  """
+  @impl CollectionStream
+  def stream(space, options \\ [], env \\ "master", api_key \\ nil) do
+    space |> CollectionStream.stream_all(&fetch_all/4, options, env, api_key)
   end
 
   defp build_single_request(space, asset, env, api_key) do

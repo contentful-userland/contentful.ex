2f0f389cbd0201b5656ee9984170bdbb324b68ea
Florian Kraft <florian.kraft@contentful.com>
2020-03-30 12:07:58 +0200

    removes old and unused stuff

--- apps/contentful_management/test/contentful/management_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Contentful.ManagementTest do
-  use ExUnit.Case
-  doctest Contentful.Management
-
-  test "greets the world" do
-    assert Contentful.Management.hello() == :world
-  end
-end
--- apps/contentful_preview/test/contentful/preview_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Contentful.PreviewTest do
-  use ExUnit.Case
-  doctest Contentful.Preview
-
-  test "greets the world" do
-    assert Contentful.Preview.hello() == :world
-  end
-end

9c3f16c57c0c26702e12442ed8040c065775727b
Florian Kraft <florian.kraft@contentful.com>
2020-04-30 16:20:31 +0200

    provides additional documentation

--- lib/contentful_delivery/entries.ex
+++ lib/contentful_delivery/entries.ex
@@ -1,6 +1,50 @@
 defmodule Contentful.Delivery.Entries do
   @moduledoc """
-  Collects functions around the reading of entries from a `Contentful.Space`
+  Entries allows for querying the entries of a space via `Contentful.Query`.
+
+  ## Fetching a single entry
+
+      import Contentful.Query
+      alias Contentful.Delivery.Entries
+
+      {:ok, entry} = Entries |> fetch_one("my_entry_id")
+
+  ## Simple querying of a collection of entries
+
+      import Contentful.Query
+      alias Contentful.Delivery.Entries
+
+      # fetches the entries based on the `space_id`, `environment` and `access_token` in config/config.exs
+      {:ok, entries, total: _total_count_of_entries} =
+        Entries |> fetch_all
+
+      # fetches the entries by passing  `space_id`, `environment` and `access_token`
+      space_id = "<my_space_id>"
+      environment = "<my_environment>"
+      access_token = "<my_access_token>"
+
+      {:ok, entries, total: _total_count_of_entries} =
+        Entries |> fetch_all(space_id, environment, access_token)
+
+  ## More advanced query with included assets
+
+  Entries can have assets included, which limits the amount of times a client has to request data from the server:
+
+      import Contentful.Query
+      alias Contentful.{Asset, Entry}
+      alias Contentful.Delivery.Entries
+
+      # The default include depth is 1 (max 10)
+      {:ok, [ %Entry{assets: assets} = entry | _ ], total: _total_count_of_entries} =
+        Entries |> include |> fetch_all
+
+      assets |> Enum.map(fn %Asset{fields: fields} -> {fields.title, fields.file} end)
+
+      # you can also just get the assets belonging to an entry lazily:
+
+      Entries |> include |> stream |> Stream.flat_map(fn entry -> entry.assets end) |> Enum.take(2)
+
+
   """
 
   alias Contentful.{Asset, Entry, Queryable, SysData}
@@ -27,21 +71,7 @@ defmodule Contentful.Delivery.Entries do
     {:ok, entries, total: total} =
       resolve_collection_response(%{"total" => total, "items" => items})
 
-    resolved_entries =
-      entries
-      |> Enum.map(fn entry ->
-        asset_ids = entry |> AssetResolver.find_linked_asset_ids()
-
-        assets_for_entry =
-          assets
-          |> Enum.map(&Assets.resolve_entity_response/1)
-          |> Enum.map(fn {:ok, asset} -> asset end)
-          |> Enum.filter(fn %Asset{sys: %SysData{id: id}} -> asset_ids |> Enum.member?(id) end)
-
-        entry |> Map.put(:assets, assets_for_entry)
-      end)
-
-    {:ok, resolved_entries, total: total}
+    {:ok, entries |> Enum.map(fn entry -> entry |> resolve_assets(assets) end), total: total}
   end
 
   @impl Queryable
@@ -64,5 +94,18 @@ defmodule Contentful.Delivery.Entries do
        fields: fields,
        sys: %SysData{id: id, revision: rev}
      }}
+  end
+
+  @spec resolve_assets(Entry.t(), list(Asset.t())) :: Entry.t()
+  defp resolve_assets(%Entry{} = entry, assets) do
+    asset_ids = entry |> AssetResolver.find_linked_asset_ids()
+
+    assets_for_entry =
+      assets
+      |> Enum.map(&Assets.resolve_entity_response/1)
+      |> Enum.map(fn {:ok, asset} -> asset end)
+      |> Enum.filter(fn %Asset{sys: %SysData{id: id}} -> asset_ids |> Enum.member?(id) end)
+
+    entry |> Map.put(:assets, assets_for_entry)
   end
 end

f4acbb33b609d9fdab957ef31fadfce8e76541d6
Christoph Grabo <christoph@contentful.com>
2020-05-11 10:24:02 +0200

    Add inch for documentation coverage

--- mix.exs
+++ mix.exs
@@ -102,7 +102,10 @@ defmodule Contentful.Mixfile do
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+
+      # docs
+      {:inch_ex, only: :docs}
     ]
   end
 

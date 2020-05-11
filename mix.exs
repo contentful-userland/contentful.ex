8dfef6d848d0bdb7059c077d8e2547ebff126806
Christoph Grabo <christoph@contentful.com>
2020-05-11 10:33:07 +0200

    Bump MSEV to 1.7
    MSEV = Minimal Support Elixir Version
    
    (Shamelessly stolen from the Rust community.)

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Contentful.Mixfile do
     [
       app: :contentful,
       version: @version,
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),

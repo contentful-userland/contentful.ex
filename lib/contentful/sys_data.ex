03e6b9de72071c6b945c11d8ca5c8cfc73704348
Florian Kraft <florian.kraft@contentful.com>
2020-06-17 08:58:04 +0200

    adds optional locale field to sys data
    The locale field is only present on the Entry and Asset types

--- lib/contentful/sys_data.ex
+++ lib/contentful/sys_data.ex
@@ -5,13 +5,15 @@ defmodule Contentful.SysData do
 
   See the [official documentation for more information](https://www.contentful.com/developers/docs/references/content-delivery-api/#/reference/locales).
   """
-  defstruct [:id, :revision, :version, :created_at, :updated_at]
+  defstruct [:id, :revision, :version, :created_at, :updated_at, locale: nil]
 
   @type t :: %Contentful.SysData{
           id: String.t(),
           revision: integer() | nil,
           version: integer() | nil,
           created_at: String.t() | nil,
-          updated_at: String.t() | nil
+          updated_at: String.t() | nil,
+          # NOTE: locale string only exists in entries and assets
+          locale: String | nil
         }
 end
